e8c8413498da2e150ea9ceb2731f80a96ce2c20c
Matthew Hassel <matthew.hassel@gmail.com>
2018-01-13 17:51:45 -0600

    let the fun begin...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './xpros.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.site = {title: "xpros.github.io"};
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header xpros-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title xpros-title">Welcome to {this.site.title}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
