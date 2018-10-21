9555d5a5bbd531b70791049687fca7abcd87dba4
Matthew Hassel <mhassel@drw.com>
2018-10-21 17:50:27 -0500

    working on adding the countdown functionality... its counting down the initial date at this time when the page first loads. The countdown timer is a bit delayed to initial begin the countdown.

--- src/countdown/countdownitem.js
+++ src/countdown/countdownitem.js
@@ -7,18 +7,18 @@ function CountdownItem(props) {
       marginLeft: "auto",
       marginRight: "auto",
       maxHeight: "100%",
-      maxWidth: "100%",
+      maxWidth: "75%",
   };
-  
+
   return(
     <div className="card text-center text-white bg-dark">
       <img className="card-img-top" alt="Countdown" src={props.imgUrl} style={imgStyle} />
-      <div className="card-header">Featured</div>
+      <div className="card-header"></div>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        <p className="card-text">Startdate: {props.startdate}</p>
+        <p className="card-text">Start Date: {props.startDate} Next Date: {props.nextDate}</p>
       </div>
-      {/*<div className="card-footer text-muted"></div>*/}
+      <div className="card-footer text-muted">COUNTDOWN TIMER => Days: {props.nextDays} - Hours: {props.nextHours} - Minutes: {props.nextMinutes} - Seconds: {props.nextSeconds}</div>
     </div>
   );
 }
--- src/countdown/countdownlist.js
+++ src/countdown/countdownlist.js
@@ -7,29 +7,25 @@ class CountdownList extends Component {
     super(props);
     console.log("props: %o", this.props);
     console.log("state: %o", this.state);
-    
+
     this.state = {};
-    
+
     //Bind functions
     this.countdownListItems = this.countdownListItems.bind(this);
-    this.onButtonClicked = this.onButtonClicked.bind(this);
   }
-  
-  onButtonClicked = () => {
-    console.log("Hello World");
-  }
-  
-  countdownListItems = () => {    
+
+  countdownListItems = () => {
     const list = this.props.countdowns.map((countdown) =>
+      countdown.display &&
       <Link key={countdown._id} to={`${this.props.match.url}/c/${countdown._id}`}>
-        <li className="list-group-item list-group-item-action list-group-item-dark" onClick={() => this.onButtonClicked()} >
+        <li className="list-group-item list-group-item-action list-group-item-dark" onClick={() => this.props.onClick(countdown._id)} >
           {countdown.name}
         </li>
       </Link>
     );
     return (list);
   }
-  
+
   render() {
     return(
       <div className="card text-white bg-dark card-outline-secondary mb-3 text-center">
@@ -43,10 +39,8 @@ class CountdownList extends Component {
           </ul>
         </div>
       </div>
-      
     )
   }
-  
 }
 
 export default CountdownList;
